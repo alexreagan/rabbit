e1199c8ab01a5e8074dc0f1303710e4a4476c1d1
AlexReagan <1569813053@qq.com>
2021-11-12 15:07:33 +0800

    [feature]: modify yangtze client

--- sdk/client.go
+++ sdk/client.go
@@ -10,14 +10,23 @@ type YangtzeClient struct {
 	Addr string `json:"addr"`
 }
 
+const ConnTimeout = 1 * time.Second
+const ReadWriteTimeout = 5 * time.Second
+
 // 根据group路径获取路径下所有的host
 func (c *YangtzeClient) GetHostsByGroup(path string) ([]*node.Host, error) {
 	uri := "/api/v1/host_group/related_hosts"
 	req := httplib.Get(c.Addr + uri)
-	req = req.SetTimeout(1*time.Second, 5*time.Second)
+	req = req.SetTimeout(ConnTimeout, ReadWriteTimeout)
 	req.Param("path", path)
 
 	var resp []*node.Host
 	err := req.ToJson(&resp)
 	return resp, err
+}
+
+func NewYangtzeClient(addr string) *YangtzeClient {
+	return &YangtzeClient{
+		Addr: addr,
+	}
 }
